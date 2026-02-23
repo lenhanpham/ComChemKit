3bc59d1d095ebf4dd4e9c208a360e4eb4814397f
lenhan <lenhanpham@outlook.com>
2026-02-23 17:49:48 +1100

    Pumped version and improved API docs

--- src/utilities/version.h
+++ src/utilities/version.h
@@ -67,7 +67,7 @@
  * Human-readable version string combining all version components.
  * Format: "vMAJOR.MINOR.PATCH"
  */
-#define COMCHEMKIT_STRING "v0.000.1"
+#define COMCHEMKIT_STRING "v0.000.2"
 
 /** @} */ // end of VersionConstants group
 
